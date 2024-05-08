780e4c35e867d92d4ff53a0bf8d46a7dea2066ad
DESKTOP-U600PV3\User <woravootbat@gmail.com>
2024-05-08 15:55:23 +0700

    🚨 add eslint with prettier

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
-import * as THREE from 'three';
+import * as THREE from "three";
 
 const scene: THREE.Scene = new THREE.Scene();
-const camera: THREE.PerspectiveCamera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera: THREE.PerspectiveCamera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000,
+);
 camera.position.z = 5;
 
 const renderer: THREE.WebGLRenderer = new THREE.WebGLRenderer();
@@ -9,15 +14,17 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 const geometry: THREE.BoxGeometry = new THREE.BoxGeometry(1, 1, 1);
-const material: THREE.MeshBasicMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const material: THREE.MeshBasicMaterial = new THREE.MeshBasicMaterial({
+  color: 0x00ff00,
+});
 const cube: THREE.Mesh = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
 function animate() {
-requestAnimationFrame(animate);
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-    renderer.render(scene, camera);
+  requestAnimationFrame(animate);
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+  renderer.render(scene, camera);
 }
 
 animate();
